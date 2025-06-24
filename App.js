2831f0649bc0b266b61b524e551fdbb17f570aad
jguasumba <jguasumba@tecnasa.com>
2025-06-24 00:22:06 -0500

    Subida inicial

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { SafeAreaView, StatusBar } from 'react-native';
-import AppNavigator from './src/AppNavigator';
+import AppNavigator from './AppNavigator';
 
 export default function App() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <StatusBar />
+      <StatusBar barStyle="dark-content" />
       <AppNavigator />
     </SafeAreaView>
   );
